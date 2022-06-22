ce8fbf332109043239a854324ae534002645b2eb
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-22 15:29:33 -0700

    switched default font

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ const Home: NextPage = () => {
   return (
     <div className='h-screen w-full grid place-items-center'>
       <div className='flex flex-col justify-center items-center'>
-        <h1 className='text-lg'>memory chimp</h1>
+        <h1>memory chimp</h1>
       </div>
     </div>
   )

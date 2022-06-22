6ef0650c48790d944778840a04a1ffd56d9a1234
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-22 15:50:59 -0700

    added nice nav icon

--- components/MobileNavBar.tsx
+++ components/MobileNavBar.tsx
@@ -1,7 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faNavicon } from "@fortawesome/free-solid-svg-icons"
+
 const MobileNavBar = () => {
   return (
-    <div className='w-full bg-green-600 h-16'>
-
+    <div className='flex flex-row-reverse pr-3 w-full h-16'>
+        <FontAwesomeIcon icon={faNavicon} className='h-full text-white'/>
     </div>
   )
 }

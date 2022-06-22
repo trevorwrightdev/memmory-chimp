353c02a5c0f95d122d5c1e0351197e2babecb3ad
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-22 15:58:53 -0700

    made navbar fixed position

--- components/MobileNavBar.tsx
+++ components/MobileNavBar.tsx
@@ -3,8 +3,8 @@ import { faNavicon } from "@fortawesome/free-solid-svg-icons"
 
 const MobileNavBar = () => {
   return (
-    <div className='flex flex-row-reverse pr-3 w-full h-16'>
-        <FontAwesomeIcon icon={faNavicon} className='h-full text-white'/>
+    <div className='fixed bg-lime-400 top-0 flex flex-row-reverse pr-3 w-full h-16'>
+        <FontAwesomeIcon icon={faNavicon} className='cursor-pointer h-full text-white'/>
     </div>
   )
 }

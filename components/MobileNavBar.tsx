d8e76b3da4dc6aeb2259f8d3de4c668a62f1b17d
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-22 15:59:46 -0700

    adjusted nav icon positioning

--- components/MobileNavBar.tsx
+++ components/MobileNavBar.tsx
@@ -3,7 +3,7 @@ import { faNavicon } from "@fortawesome/free-solid-svg-icons"
 
 const MobileNavBar = () => {
   return (
-    <div className='fixed bg-lime-400 top-0 flex flex-row-reverse pr-3 w-full h-16'>
+    <div className='fixed bg-lime-400 top-0 flex flex-row-reverse pr-2 w-full h-16'>
         <FontAwesomeIcon icon={faNavicon} className='cursor-pointer h-full text-white'/>
     </div>
   )

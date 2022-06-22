36508086424d2d63c77873eb0c123bf1972eff60
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-22 11:19:24 -0700

    added heading

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      
+    <div className='h-screen w-full grid place-items-center'>
+      <div className='flex flex-col justify-center items-center'>
+        <h1 className='text-lg'>memory chimp</h1>
+      </div>
     </div>
   )
 }

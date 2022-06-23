94cc972009f39ef13d9acfdba9e1ce143a8f78db
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-22 17:19:02 -0700

    removed user-select for anchor tag

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { faCode } from "@fortawesome/free-solid-svg-icons"
 const Footer = () => {
   return (
     <div className='flex flex-row justify-center items-center w-full h-20 absolute bottom-0'>
-        <a href="https://github.com/trevorwrightdev/memory-chimp" target="_blank" rel="noopener noreferrer">
+        <a className='select-none' href="https://github.com/trevorwrightdev/memory-chimp" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faCode} /> GitHub
         </a>
     </div>

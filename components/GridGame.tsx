1057ff9c5f01205d61e686e0c5df855301f501b1
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-22 18:27:20 -0700

    made function to get grid items using typescript stuff

--- components/GridGame.tsx
+++ components/GridGame.tsx
@@ -1,12 +1,25 @@
-const GridItem = () => {
+interface GridProps {
+  number?: number,
+  active?: boolean,
+}
+
+const GridItem = (props: GridProps) => {
   return <div className='bg-lime-600 h-full w-full rounded-lg' />
 }
 
 const GridGame = () => {
 
+  const getGridItems = (): JSX.Element[] => {
+    const items: JSX.Element[] = []
+    for (let i = 0; i < 24; i++) {
+      items.push(<GridItem />)
+    }
+    return items
+  }
+
   return (
-    <div className='p-3 gap-2 grid grid-cols-4 grid-rows-6 w-[350px] h-[550px] bg-lime-500 rounded-lg'>
-      <GridItem />
+    <div className={`p-3 gap-2 grid grid-cols-4 grid-rows-6 w-[350px] h-[550px] bg-lime-500 rounded-lg`}>
+      {getGridItems()}
     </div>
   )
 }

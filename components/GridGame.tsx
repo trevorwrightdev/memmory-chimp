9f79264bf582dfa30422504990715defe4acf943
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-22 19:29:04 -0700

    set up fade transition

--- components/GridGame.tsx
+++ components/GridGame.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { motion } from 'framer-motion'
 
 interface GridProps {
   number?: number,
@@ -15,6 +16,9 @@ const GridItem = (props: GridProps): JSX.Element => {
 
 const GridGame = (): JSX.Element => {
 
+  const [fadeMenu, setFadeMenu] = useState<boolean>(false)
+  const [removeMenu, setRemoveMenu] = useState<boolean>(false)
+
   const getGridItems = (): JSX.Element[] => {
     const items: JSX.Element[] = []
     for (let i = 0; i < 35; i++) {
@@ -23,14 +27,23 @@ const GridGame = (): JSX.Element => {
     return items
   }
 
+  const startGame = (): void => {
+    console.log('running!')
+    setFadeMenu(true)
+
+    setTimeout(() => {
+      setRemoveMenu(true)
+    }, 500)
+  }
+
   return (
     <div className='w-[350px] h-[550px] bg-lime-500 rounded-lg relative'>
       <div className='place-items-center w-full h-full p-3 gap-2 grid grid-cols-5 grid-rows-7 absolute'>
         {getGridItems()}
       </div>
-      <div className='flex flex-col justify-center items-center w-full h-full bg-black/20 absolute rounded-lg'>
-        <h1 className='text-lg bg-lime-400 p-1 rounded-lg'>tap to play</h1>
-      </div>
+      {!removeMenu && <motion.div animate={fadeMenu && { opacity: 0 }} transition={{duration: 0.5}} className='grid place-items-center w-full h-full bg-black/20 absolute rounded-lg'>
+        <div onClick={!fadeMenu ? startGame : undefined} className='cursor-pointer text-lg bg-lime-400 p-1 rounded-lg'>tap to play</div>
+      </motion.div>}
     </div>
   )
 }

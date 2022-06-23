cce383fb500eba7ea19d4fac7251873be282d260
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-22 18:14:15 -0700

    added the grid

--- components/GridGame.tsx
+++ components/GridGame.tsx
@@ -1,7 +1,31 @@
 const GridGame = () => {
+
   return (
-    <div className='w-[350px] h-[550px] bg-lime-500 rounded-lg'>
-      
+    <div className='p-3 gap-2 grid grid-cols-4 grid-rows-6 w-[350px] h-[550px] bg-lime-500 rounded-lg'>
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <div className='bg-lime-600 h-full w-full rounded-lg' />
     </div>
   )
 }

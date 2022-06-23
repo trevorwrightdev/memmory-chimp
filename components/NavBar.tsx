e3c457234f3806172ec0ae211564ba51e9d6dea6
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-23 11:51:37 -0700

    mobile nav now works

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,6 +3,7 @@ import { faNavicon } from "@fortawesome/free-solid-svg-icons"
 import { motion } from "framer-motion"
 import { faBorderAll } from "@fortawesome/free-solid-svg-icons"
 import { faCircle } from "@fortawesome/free-solid-svg-icons"
+import { useRouter } from "next/router"
 
 export interface Props {
   setMenu: (val: boolean) => void
@@ -10,6 +11,13 @@ export interface Props {
 }
 
 const NavBar = (props: Props): JSX.Element => {
+  const router = useRouter()
+
+  const redirect = (path: string): void => {
+    props.setMenu(false)
+
+    router.push('/' + path)
+  }
 
   return (
     <>
@@ -22,22 +30,24 @@ const NavBar = (props: Props): JSX.Element => {
             <div className='w-[45px] h-full'/>
         </div>
         <motion.div
-          animate={props.menuOpen ? { height: 'calc(100vh - 5rem)' } : { height: '0vh' }} 
+          animate={props.menuOpen ? { height: 'calc(100vh - 5rem)' } : { height: '0px' }} 
           transition={{duration: 0.25}}
-          className='bg-lime-400 w-full grid grid-cols-3 grid-rows-3 p-5'
+          className='bg-lime-400 w-full grid grid-cols-3 grid-rows-3'
         >
-          <div className='flex flex-col justify-center items-center'>
-            <div className='bg-lime-600 w-24 rounded-lg aspect-square grid place-items-center'>
-              <FontAwesomeIcon className='text-[32px]' icon={faBorderAll} />
+          {props.menuOpen && <>
+            <div onClick={() => redirect('grid')} className='flex flex-col justify-center items-center'>
+              <div className='bg-lime-600 w-24 rounded-lg aspect-square grid place-items-center'>
+                <FontAwesomeIcon className='text-[32px]' icon={faBorderAll} />
+              </div>
+              <h1>grid</h1>
             </div>
-            <h1>grid</h1>
-          </div>
-          <div className='flex flex-col justify-center items-center'>
-            <div className='bg-lime-600 w-24 rounded-lg aspect-square grid place-items-center'>
-              <FontAwesomeIcon className='text-[32px]' icon={faCircle} />
+            <div onClick={() => redirect('pi')} className='flex flex-col justify-center items-center'>
+              <div className='bg-lime-600 w-24 rounded-lg aspect-square grid place-items-center'>
+                <FontAwesomeIcon className='text-[32px]' icon={faCircle} />
+              </div>
+              <h1>pi</h1>
             </div>
-            <h1>pi</h1>
-          </div>
+          </>}
         </motion.div>
       </div>
       <div className='h-20'/>

8a91e3d446893e2d6b4c6bad4fb8b07f3e815ad0
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-22 18:17:04 -0700

    made grid item component within gridgame tsx

--- components/GridGame.tsx
+++ components/GridGame.tsx
@@ -1,31 +1,12 @@
+const GridItem = () => {
+  return <div className='bg-lime-600 h-full w-full rounded-lg' />
+}
+
 const GridGame = () => {
 
   return (
     <div className='p-3 gap-2 grid grid-cols-4 grid-rows-6 w-[350px] h-[550px] bg-lime-500 rounded-lg'>
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
-      <div className='bg-lime-600 h-full w-full rounded-lg' />
+      <GridItem />
     </div>
   )
 }

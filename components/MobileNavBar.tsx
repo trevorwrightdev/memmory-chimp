62e5e42bdced0cda3e9584cd3e9f099877f2ac99
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-22 17:48:59 -0700

    changed text and icon color to black

--- components/MobileNavBar.tsx
+++ components/MobileNavBar.tsx
@@ -5,7 +5,7 @@ const MobileNavBar = () => {
   return (
     <>
         <div className='select-none fixed bg-lime-400 top-0 flex flex-row-reverse p-4 w-full h-20'>
-            <FontAwesomeIcon icon={faNavicon} className='cursor-pointer h-full text-white'/>
+            <FontAwesomeIcon icon={faNavicon} className='cursor-pointer h-full'/>
         </div>
         <div className='h-20'/>
     </>
